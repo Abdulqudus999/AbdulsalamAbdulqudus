f5bf60c475201c1a416b3ef3fda41bfef5b0d8bf
Abdulqudus999 <abdulsalamolaide958@gmail.com>
2025-05-11 10:42:56 +0100

    adjusted

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Loader = ({isLoading}) => {
   return (
     <div className={`fixed inset-0 bg-white dark:bg-gray-900 z-50 grid place-items-center transition-opacity duration-1000 ease-in-out ${isLoading ? 'opacity-100' : 'opacity-0 pointer-events-none'}`}>
-      <img src="../assests/loading-gif-1.gif" alt="loader" className='w-40' />
+      <img src="./src/assests/loading-gif-1.gif" alt="loader" className='w-40' />
       
     </div>
   )
